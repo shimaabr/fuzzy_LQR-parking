46aa0075df0e619886db47ee798ff02132ef57ea
shimaabr <136001265+shimaabr@users.noreply.github.com>
2025-10-08 02:56:45 +0330

    Add files via upload

--- car.m
+++ car.m
@@ -1,47 +1,51 @@
 function u = car(state, R_fuzzy, Q_fuzzy,state_ref)
-% Adaptive Q/R LQR controller
-% Inputs:
-%   x         - 5x1 state vector [x; y; theta; phi; v]
-%   R_fuzzy   - scalar weight from fuzzy for R
-%   Q_fuzzy   - scalar weight from fuzzy for Q
-% Output:
-%   u         - 2x1 control vector [a_cmd; phi_dot_cmd]
-
-    %#codegen
-
     %% Parameters
     L = 2.5;       % wheelbase [m]
-    v0 = 1;        % operating velocity
-    theta0 = .2;    % operating orientation
-    phi0=0;
+    
+     v0 = state(5);
+     theta0 = state(3);
+     phi0 = state(4);
+    phi0 = max(min(phi0, pi/6), -pi/6);
 
-    %% Linearized system around (theta0, v0)
-    A = [0 0 -v0*sin(theta0) 0 cos(theta0);
-         0 0  v0*cos(theta0) 0 sin(theta0);
-         0 0  0 (v0/L)* (1/cos(phi0)^2) tan(phi0)/L;
-         0 0  0 0 0;
-         0 0  0 0 0];
+    %% Linearized system around (theta, v)
+  
 
-    B = [0 0;
-         0 0;
-         0 0;
-         0 1;
-         1 0];
+A = [  0,  0, -v0*sin(theta0),           0,             cos(theta0);
+       0,  0,  v0*cos(theta0),           0,             sin(theta0);
+       0,  0,          0,   (v0/L)*(1/cos(phi0)^2),     (1/L)*tan(phi0);
+       0,  0,          0,               0,                     0;
+       0,  0,          0,               0,                     0 ];
+
+B = [  0,  0;
+       0,  0;
+       0,  0;
+       0,  1;
+       1,  0 ];
+
 
     %% Base Q and R
-    Q_default = eye(5);   % 5x5, for 5 states
-    R_default = eye(2);   % 2x2, for 2 inputs
+    Q_default =[1 0 0 0 0
+                0 50 0 0 0 
+                0 0 100 0 0 
+                0 0 0 1 0 
+                0 0 0 0 1];   %  for 5 states
+    R_default = eye(2);   %  for 2 inputs
 
     %% Adaptive scaling from fuzzy outputs
     Q_new = Q_fuzzy * Q_default;
     R_new = R_fuzzy * R_default;
 
-   %% Solve LQR
-    K = lqr(A, B, Q_new, R_new);
+     %% Solve LQR safely
+        K = lqr(A, B, Q_new, R_new);
+   
+
 
     % Error state
     e = state - state_ref;
 
     % Control input
-    u = -K * e;
+     u = -K * e;
+    u(1) = max(min(u(1), .53), -.53); % فرمان
+u(2) = max(min(u(2), 1), 0);     % شتاب
+    
 end
--- car_LQR.m
+++ car_LQR.m
@@ -1,1 +1,44 @@
+function u = car(state,state_ref)
+    %% Parameters
+    L = 2.5;       % wheelbase [m]
+     v0 = state(5);
+     theta0 = state(3);
+     phi0 = state(4);
+phi0 = max(min(phi0, pi/6), -pi/6);
 
+  %% Linearized system around (theta, v)
+  
+
+A = [  0,  0, -v0*sin(theta0),           0,             cos(theta0);
+       0,  0,  v0*cos(theta0),           0,             sin(theta0);
+       0,  0,          0,   (v0/L)*(1/cos(phi0)^2),     (1/L)*tan(phi0);
+       0,  0,          0,               0,                     0;
+       0,  0,          0,               0,                     0 ];
+
+B = [  0,  0;
+       0,  0;
+       0,  0;
+       0,  1;
+       1,  0 ];
+
+    %% Base Q and R
+    Q_default =[1 0 0 0 0
+                0 50 0 0 0 
+                0 0 100 0 0 
+                0 0 0 1 0 
+                0 0 0 0 1];   %  for 5 states
+    R_default = eye(2);   %  for 2 inputs
+
+   
+
+   %% Solve LQR
+    K = lqr(A, B, Q_default, R_default);
+
+    % Error state
+    e = state - state_ref;
+
+    % Control input
+    u = -K * e;
+    u(1) = max(min(u(1), .53), -.53); % فرمان
+u(2) = max(min(u(2), 1), 0);     % شتاب
+end
